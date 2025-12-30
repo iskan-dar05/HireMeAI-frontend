d89ef393509b2ef98351cb1e402fc7c078eb2990
iskandar boukhenoufa <iskandar@iskandar.iskandar@skou.com>
2025-12-29 19:09:41 -0500

    fix redirection

--- src/axios.ts
+++ src/axios.ts
@@ -1,3 +1,35 @@
+import axios from "axios";
+
+const API_URL = "https://hiremeai-backend.onrender.com/";
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  withCredentials: false, // Important: false for Bearer token auth
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      console.log("TOKEN::::::", token);
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    
+    // Ensure Content-Type is set for all requests
+    if (!config.headers["Content-Type"]) {
+      config.headers["Content-Type"] = "application/json";
+    }
+    
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
