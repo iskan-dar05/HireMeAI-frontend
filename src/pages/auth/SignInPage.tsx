a40e5aea5a2b6d51a873090f6d66345de25d11a2
iskandar boukhenoufa <iskandar@iskandar.iskandar@skou.com>
2026-01-04 16:29:20 -0500

    complete style

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,5 +1,5 @@
 import { SignIn, useTheme } from "@clerk/clerk-react";
-import { dark, light } from '@clerk/themes'
+import { dark, light, neobrutalism } from '@clerk/themes'
 import { useContext, useEffect, useState } from "react"
 import { ThemeContext } from "@/lib/theme-context"
 const SignInPage = () => {
@@ -11,24 +11,33 @@ const SignInPage = () => {
   }, [theme]);
 
   return (
-    <div className="min-h-[calc(100vh-100px)] flex items-center justify-center bg-card-primary dark:bg-card-secondary">
+    <div className="min-h-[calc(100vh-100px)] bg-background flex items-center justify-center">
       <div className="w-2/4 flex items-center flex-col rounded-2xl shadow-custom-2xl animate-slide-in-up p-6">
         {/* Logo */}
-        <div className="flex items-center gap-2 mb-4">
-          <div className="w-8 h-8 flex items-center justify-center rounded-custom bg-accent shadow-float">
-            <span className="text-white font-bold text-lg">H</span>
+        <div className="flex items-center gap-2">
+          <div className="w-8 h-8 flex items-center justify-center rounded-lg bg-gradient-to-br from-accent to-primary animate-float shadow-float">
+            <span className="dark:text-gray-900 text-white font-bold text-lg">H</span>
           </div>
-          <span className="font-bold text-lg bg-accent bg-clip-text text-transparent">
+          <span className="font-bold text-xl bg-gradient-to-r from-accent to-primary bg-clip-text text-transparent">
             HireMeAI
           </span>
         </div>
 
         <SignIn
-          path="/signin"
-          routing="path"
-          signUpUrl="/signup"
-          appearance={{ theme: clerkTheme }} // <-- apply dark/light mode
-        />
+        path="/signin"
+        routing="path"
+        signUpUrl="/signup"
+        appearance={{
+          theme: clerkTheme,
+          elements: {
+            formButtonPrimary: {
+              color: "black",
+              fontWeight: "500",
+            },
+          },
+        }}
+      />
+
       </div>
     </div>
   );
--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -12,14 +12,14 @@ const SignUpPage = () => {
   }, [theme]);
 
   return (
-    <div className="min-h-[calc(100vh-100px)] flex items-center justify-center bg-card-primary dark:bg-card-secondary">
+    <div className="min-h-[calc(100vh-100px)] flex items-center justify-center bg-background">
       <div className="w-2/4 flex items-center flex-col rounded-2xl shadow-custom-2xl animate-slide-in-up p-6">
         {/* Logo */}
-        <div className="flex items-center gap-2 mb-4">
-          <div className="w-8 h-8 flex items-center justify-center rounded-custom bg-accent shadow-float">
-            <span className="text-white font-bold text-lg">H</span>
+        <div className="flex items-center gap-2">
+          <div className="w-8 h-8 flex items-center justify-center rounded-lg bg-gradient-to-br from-accent to-primary animate-float shadow-float">
+            <span className="dark:text-gray-900 text-white font-bold text-lg">H</span>
           </div>
-          <span className="font-bold text-lg bg-accent bg-clip-text text-transparent">
+          <span className="font-bold text-xl bg-gradient-to-r from-accent to-primary bg-clip-text text-transparent">
             HireMeAI
           </span>
         </div>
